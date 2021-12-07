052548e2f61fcee67ed18f5b2f2973d9728d2bb5
Ricardo Silva Manayalle <ricardo@ricardonotes.com>
2021-12-07 12:08:27 -0500

    Gatsby's tutorial finished

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,20 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "ricardonotes",
+    siteUrl: "https://www.ricardonotes.com",
+    title: "Ricardo's blog",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
 };
